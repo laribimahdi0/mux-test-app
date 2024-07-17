36e4209ad194f0a4c1fe155c88c0cdd77e4ed940
mahdi <laribimahdi1@gmail.com>
2024-07-17 17:31:11 +0100

    create  video detail form

---
+++ src/app/asset/[videoId]/page.js
@@ -0,0 +1,40 @@
+import React from "react";
+import MuxPlayer from "@mux/mux-player-react";
+import Mux from "@mux/mux-node";
+import VideoDetailForm from "@/components/VideoDetailForm";
+
+async function video({ params: { videoId } }) {
+  console.log("====>", videoId);
+
+  const { video } = new Mux(
+    process.env.MUX_TOKEN_ID,
+    process.env.MUX_TOKEN_SECRET
+  );
+
+  const asset = await video.assets.retrieve(videoId);
+
+  const playbackIds = asset.playback_ids;
+  const playbackId = playbackIds.find((id) => id.policy === "public");
+
+
+  return (
+    <div className="h-screen p-6">
+      <h3 className="text-2xl mb-6">Video Detail</h3>
+      <div className="w-96">
+        <div className="bg-black aspect-video mb-8 -mx-4 flex shadow-xl">
+          <MuxPlayer
+            className="w-full"
+            playbackId={playbackId?.id}
+            accentColor="rgb(220 38 38)"
+            streamType="on-demand"
+          />
+        </div>
+
+        <VideoDetailForm playbackId ={playbackId?.id} />
+
+      </div>
+    </div>
+  );
+}
+
+export default video;
---
+++ src/components/VideoDetailForm.js
@@ -0,0 +1,42 @@
+"use client";
+import React, { useState } from "react";
+import Button from "./Button";
+import Input from "./Input";
+import { addMedia } from "@/lib/indexDB";
+import withAuth from "@/lib/withAuth";
+import { useRouter } from 'next/navigation'
+
+function VideoDetailForm({playbackId}) {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const  router =  useRouter()
+
+  const handleSubmit = async() => {
+    const media = { title, description  , playbackId};
+   await addMedia(media);
+   router.push("/")
+  };
+
+  return (
+    <div className="flex flex-col space-y-6 w-full">
+      <Input
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        placeholder="Titre"
+        required
+      />
+      <Input
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+        className="p-2"
+        type="textarea"
+        placeholder="Description"
+      />
+      <Button onClick={handleSubmit} type={"primary"}>
+        Enregister
+      </Button>
+    </div>
+  );
+}
+
+export default withAuth(VideoDetailForm);
